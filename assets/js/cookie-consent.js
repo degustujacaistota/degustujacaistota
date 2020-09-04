0d8b429150dda5b7691f67d5fc87ccfeeb504f5c
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-09-04 20:14:37 +0200

    Update assets

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    function createExpires(days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        return "; expires=" + date.toUTCString();
+    }
+    function createCookie(name,value,days) {
+        const expires = days ? createExpires(days) : "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+    function readCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            let c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+    function eraseCookie(name) {
+        createCookie(name,"",-1);
+    }
+    function isTrue(value) {
+        return value == 'true'
+    }
+    const cookieNoticeDismissedKey = 'cookie-notice-dismissed';
+    const cookieNoticeDismissed = isTrue(readCookie(cookieNoticeDismissedKey));
+    const cookieNotice = $('#cookie-notice');
+    if(!cookieNoticeDismissed) {
+        cookieNotice.show();
+    }
+    $('#cookie-notice-show').click(function() {
+        cookieNotice.show();
+    });
+    $('#cookie-notice-accept').click(function() {
+        createCookie(cookieNoticeDismissedKey,'true',31);
+        cookieNotice.hide();
+    });
+});
